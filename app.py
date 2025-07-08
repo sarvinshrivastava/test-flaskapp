535b759cecd16a7cc94a2d14890069038a325b60
Sarvin Shrivastava <41331485+sarvinshrivastava@users.noreply.github.com>
2025-07-08 12:35:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import mysql.connector
 app = Flask(__name__)
 
 db = mysql.connector.connect(
-    host="database-1.c2f46y28s2vw.us-east-1.rds.amazonaws.com",
+    host="my-project-db.c3eykuyoapd0.ap-south-1.rds.amazonaws.com",
     user="admin",
     password="Test#123",
     database="myprojectdb"
